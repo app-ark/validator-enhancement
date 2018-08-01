6c3632510dc861d0e3d4889b82db4c6a67d9aece
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2018-08-01 18:06:49 +0800

    Create ValidationException.php

---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 异常
+ *
+ * @category Validator
+ * @package  Validator
+ * @author   xiaohui lam <xiaohui.lam@icloud.com>
+ * @license  MIT LICENSE
+ * @link     https://github.com/laravelfy/validator-enhancement
+ */
+namespace Laravelfy\Validator\Exceptions;
+
+use Illuminate\Validation\ValidationException as BaseException;
+
+/**
+ * 异常
+ *
+ * @category Validator
+ * @package  Validator
+ * @author   xiaohui lam <xiaohui.lam@icloud.com>
+ * @license  MIT LICENSE
+ * @link     https://github.com/laravelfy/validator-enhancement
+ */
+class ValidationException extends BaseException
+{
+}
